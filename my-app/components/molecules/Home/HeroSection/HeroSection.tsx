53d1e48821fc0c077da443762e61d5de3884d548
MASUR89 <surmava9@gmail.com>
2026-01-28 14:41:40 +0400

    3 lines

--- my-app/components/molecules/Home/HeroSection/HeroSection.tsx
+++ my-app/components/molecules/Home/HeroSection/HeroSection.tsx
@@ -2,7 +2,7 @@ import HomePageButton from '@/components/atoms/HomePageButton/HomePageButton'
 
 function HeroSection() {
   return (
-    <div>
+    <div className='w-screen h-auto flex flex-col'>
       
         <div className='h-[650px] w-screen flex'>
           <div className="bg-black text-white w-[610px] h-[650px] flex justify-center items-center">
@@ -32,26 +32,62 @@ function HeroSection() {
           </div>
         </div>
 
-        {/* <div>
-          <section className="bg-black text-white p-20">
-            <h1 className="text-4xl font-bold tracking-wide">
-              CREATE AND SHARE YOUR PHOTO STORIES.
-            </h1>
+        <div className='h-[650px] w-screen flex'>
+          <div className="w-[830px] h-[650px] ">
+            <img
+            src='/images/N2.svg' 
+            className='w-full h-full object-cover'/>
+          </div>
+          <div className="bg-white text-black w-[610px] h-[650px] flex justify-center items-center">
+            <div className='w-[387px] h-[329px]'>
+                <h1 className="text-[40px] font-bold tracking-[4.17px] leading-[48px]">
+                  BEAUTIFUL STORIES <br />EVERY TIME
+                </h1>
 
-            <p className="mt-6 text-sm max-w-sm opacity-70">
-              Photosnap is a platform for photographers and visual storytellers.
-            </p>
+                <p className="mt-4 text-[15px] font-normal leading-[25px] opacity-60">
+                  We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.
+                </p>
 
-            <div className="mt-10">
-              <HomePageButton
-                text="GET AN INVITE"
-                textColor="white"
-                arrowColor="white"
-                gap={4.5}
-              />
+                <div className="mt-10 bottom-0">
+                  <HomePageButton
+                    text="VIEW THE STORIES"
+                    textColor="black"
+                    arrowColor="black"
+                    gap={4.5}
+                  />
+                </div> 
             </div>
-          </section>
-        </div> */}
+          </div>
+        </div>
+
+
+        <div className='h-[650px] w-screen flex'>
+          <div className="bg-white text-black w-[610px] h-[650px] flex justify-center items-center">
+            <div className='w-[387px] h-[304px]'>
+                <h1 className="text-[40px] font-bold tracking-[4.17px] leading-[48px]">
+                  DESIGNED FOR EVERYONE
+                </h1>
+
+                <p className="mt-4 text-[15px] font-normal leading-[25px] opacity-60">
+                  Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it. 
+                </p>
+
+                <div className="mt-10 bottom-0">
+                  <HomePageButton
+                    text="VIEW THE STORIES"
+                    textColor="black"
+                    arrowColor="black"
+                    gap={4.5}
+                  />
+                </div> 
+            </div>
+          </div>
+          <div className="w-[830px] h-[650px] ">
+            <img
+            src='/images/N3.svg' 
+            className='w-full h-full object-cover'/>
+          </div>
+        </div>
         
     
     </div>
