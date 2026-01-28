94829983d9fb9d095c4fb013b4db94532bc82f94
MASUR89 <surmava9@gmail.com>
2026-01-28 22:13:25 +0400

    started boxes

--- my-app/components/molecules/Home/Benefits/Benefits.tsx
+++ my-app/components/molecules/Home/Benefits/Benefits.tsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 function Benefits() {
   return (
-    <div>Benefits</div>
+    <div className='w-screen h-115 flex justify-center items-center'>
+        <div className='h-59 w-277.5 flex justify-between'>
+
+           {/* box1 */}
+            <div className="w-87.5 h-full flex flex-col justify-between items-center">
+                <div className='w-18 h-18 flex items-center'>
+                    <img src="/images/Shape1.svg" alt="" />
+                </div>
+                <div className='w-full h-29 text-center'>
+                    <p>100% Responsive</p>
+                    <p>No matter which the device you’re on, our site is fully responsive and stories look beautiful on any screen.</p>
+                </div> 
+            </div> 
+
+            {/* box2 */}
+            <div className="w-87.5 h-full">
+
+            </div> 
+
+            {/* box3 */}
+            <div className="w-87.5 h-full">
+
+            </div> 
+        </div>  
+    </div>
   )
 }
 
