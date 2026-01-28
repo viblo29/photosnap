a201e2939a355b3b0d63422f4a421d10a5710d5a
viblo29 <dachivibliani29@gmail.com>
2026-01-28 21:12:22 +0400

    fix/homePage-Button
    added underline hover effect, also fixed svg

--- my-app/components/atoms/HomePageButton/HomePageButton.tsx
+++ my-app/components/atoms/HomePageButton/HomePageButton.tsx
@@ -1,26 +1,23 @@
+
 import React from 'react'
+import RightArrowIcon from '../RightArrowIcon/RightArrowIcon'
 
 interface HomePageButtonProps {
   text: string
   textColor: 'white' | 'black'
-  arrowColor: 'white' | 'black'
   gap: 4.5 | 6.5
 }
 
-function HomePageButton({ text, textColor, arrowColor, gap }: HomePageButtonProps) {
+function HomePageButton({ text, textColor, gap }: HomePageButtonProps) {
   const textColorClass = textColor === 'white' ? 'text-white' : 'text-black'
   const gapClass = gap === 4.5 ? 'gap-4.5' : 'gap-6.5'
-  const strokeColor = arrowColor === 'white' ? 'white' : 'black'
 
   return (
     <div>
-        <button className={`font-bold text-xs tracking-[2px] flex justify-center cursor-pointer ${gapClass} ${textColorClass}`}>
+        <button className={`font-bold text-xs tracking-[2px] flex justify-center cursor-pointer hover:underline ${gapClass} ${textColorClass}`}>
           {text}
             <div>
-                <svg width="42" height="13" viewBox="0 0 42 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0 6.35355H41.864" stroke={strokeColor}/>
-                <path d="M35.4282 0.353546L41.4282 6.35355L35.4282 12.3535" stroke={strokeColor}/>
-                </svg>
+                <RightArrowIcon arrowColor={textColor}/>
             </div>
         </button>
     </div>
