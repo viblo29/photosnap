be4e45d77c1447bc7d6346529b6cdf96a55b1bc3
sandro gegechkori <gegechkorisandro1@gmail.com>
2026-01-29 20:10:33 +0400

    fix/ fixed arrow button

--- my-app/components/atoms/StoryButton/StoryButton.tsx
+++ my-app/components/atoms/StoryButton/StoryButton.tsx
@@ -1,22 +1,19 @@
 import React from 'react'
 
+interface HomePageButtonProps {
+  width: string
+}
 
-function StoryButton() {
-    return (
-        <div >
-            <button
-                className={`font-bold text-xs tracking-[2px] flex justify-between items-center text-white`}
-                style={{ width: `280px` }}
-            >
-                READ STORY
-                <svg width="42" height="13" viewBox="0 0 42 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 6.35355H41.864" stroke="white" />
-                    <path d="M35.4282 0.353546L41.4282 6.35355L35.4282 12.3535" stroke="white" />
-                </svg>
-            </button>
-
-        </div>
-    )
+function StoryButton({ width }: HomePageButtonProps) {
+  return (
+    <button
+      style={{ width: width }}
+      className="font-bold text-xs tracking-[2px] flex items-center  text-white cursor-pointer justify-between"
+    >
+      READ STORY
+      <img src="/images/story_arrow.svg" alt="arrow" />
+    </button>
+  )
 }
 
 export default StoryButton
