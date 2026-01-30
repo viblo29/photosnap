1bbd9713a9a368afc9c9fe283c8983cc289d86f3
viblo29 <dachivibliani29@gmail.com>
2026-01-30 17:48:06 +0400

    feat/mobile-header-menu

--- my-app/components/molecules/Home/Header/Header.tsx
+++ my-app/components/molecules/Home/Header/Header.tsx
@@ -1,23 +1,29 @@
 import Image from 'next/image'
-import React from 'react'
 import siteLogo from '../../../../public/photosnap-black.svg'
 import NavButtons from '@/components/atoms/NavButtons/NavButtons'
+import MobileMenu from './MobileMenu'
+
 function Header() {
   return (
-    <div>
-      <div className='w-full h-18 flex justify-around items-center'>
-        <div className='cursor-pointer p-1 transition-opacity duration-300 hover:opacity-70'>
-          <Image src={siteLogo} alt='Website Logo'/>
+    <>
+      <div className='hidden md:block'>
+        <div className='w-full h-18 flex justify-around items-center'>
+          <div className='cursor-pointer p-1 transition-opacity duration-300 hover:opacity-70'>
+            <Image src={siteLogo} alt='Website Logo'/>
+          </div>
+          <div className='flex gap-9.25 '>
+            <NavButtons text="Stories" textColor="black"/>
+            <NavButtons text="FEATURES" textColor="black"/>
+            <NavButtons text="PRICING" textColor="black"/>
+          </div>
+          <button className='font-bold text-xs tracking-[2px] px-6 py-3 bg-[#000000] text-white hover:text-black hover:bg-[#DFDFDF] cursor-pointer transition-colors duration-300 ease-in-out'>
+            GET AN INVITE
+          </button>
         </div>
-        <div className='flex gap-9.25 '>
-          <NavButtons text="Stories" textColor="black"/>
-          <NavButtons text="FEATURES" textColor="black"/>
-          <NavButtons text="PRICING" textColor="black"/>
-
-        </div>
-        <button className=' font-bold text-xs tracking-[2px] px-6 py-3 bg-[#000000] text-white hover:text-black hover:bg-[#DFDFDF] cursor-pointer transition-colors duration-300 ease-in-out'>GET AN INVITE</button>
       </div>
-    </div>
+
+      <MobileMenu />
+    </>
   )
 }
 
